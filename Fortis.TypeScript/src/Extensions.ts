fdb847780576570d2ce3f7e4b27a8869a660ef96
Huw Simpson <huwsimpson@gmail.com>
2015-09-26 19:51:29 +0200

    Fortis.Typescript: added convenient functions in extensions.ts

---
+++ Fortis.TypeScript/src/Extensions.ts
@@ -0,0 +1,48 @@
+﻿"use strict";
+
+interface Array<T> {
+    pick<TValueOut>(choice: (item: T) => Option<TValueOut>): Option<TValueOut>
+    choose<TValueOut>(choice: (item: T) => Option<TValueOut>): Option<TValueOut>[]
+}
+
+function pick<TValue, TValueOut>(items: TValue[], choice: (item: TValue) => Option<TValueOut>): Option<TValueOut> {
+    if (choice === undefined && choice === null) {
+        throw new Error("'choice' undefined or null!");
+    }
+
+    for (let item of items) {
+        let res = choice(item);
+        if (res.exists) {
+            return res;
+        }
+    }
+
+    return Option.none<TValueOut>();
+}
+
+Array.prototype.pick = function <TValue, TValueOut>(choice: (item: TValue) => Option<TValueOut>): Option<TValueOut> {
+    return pick(this, choice);
+}
+
+function choose<TValue, TValueOut>(items: TValue[], choice: (item: TValue) => Option<TValueOut>): Option<TValueOut>[] {
+    if (choice === undefined && choice === null) {
+        throw new Error("'choice' undefined or null!");
+    }
+
+    return items.map(choice).filter(item => item.exists);
+}
+
+Array.prototype.choose = function <TValue, TValueOut>(choice: (item: TValue) => Option<TValueOut>): Option<TValueOut>[] {
+    return choose(this, choice);
+}
+
+function tryParseInt(value: string, radix?:number): Option<number> {
+    let res = parseInt(value, radix);
+
+    return (isNaN(res)) ? Option.none<number>() : Option.ofValue<number>(res);
+}
+
+function tryParseFloat(value: string): Option<number> {
+    let res = parseFloat(value);
+    return (isNaN(res)) ? Option.none<number>() : Option.ofValue<number>(res);
+}
